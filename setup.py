42d78dc9d5e6fc4980c8b753319686a1114fbc82
vimal-11 <vimalaadith.bs@gmail.com>
2022-02-07 23:49:10 +0530

    multiple pyd files

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup
+from setuptools import Extension, setup
 from Cython.Build import cythonize
+
+extensions = [Extension("*", ["*.pyx"])]
 
 setup(
     name="My shared object",
-    ext_modules = cythonize("Shared_objects/*.pyx")
+    ext_modules = cythonize(extensions)
 )
